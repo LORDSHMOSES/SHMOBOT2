20d5b1969d38594286fe2966a39f1efd9aac8381
Nathan Erickson <33501532+LORDSHMOSES@users.noreply.github.com>
2021-02-01 22:49:59 -0600

    This is bootlegged as hell but idgaf`

---
+++ r6sapi/platforms.py
@@ -0,0 +1,35 @@
+"""
+Copyright (c) 2016-2019 billyoyo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+class Platforms:
+    """Platforms supported
+
+    Attributes
+    ----------
+    UPLAY : str
+        name of the uplay platform
+    XBOX : str
+        name of the xbox platform
+    PLAYSTATION : str
+        name of the playstation platform"""
+
+    UPLAY = "uplay"
+    XBOX = "xbl"
+    PLAYSTATION = "psn"
+
+
+valid_platforms = [x.lower() for x in dir(Platforms) if "_" not in x]
+
+
+PlatformURLNames = {
+    "uplay": "OSBOR_PC_LNCH_A",
+    "psn": "OSBOR_PS4_LNCH_A",
+    "xbl": "OSBOR_XBOXONE_LNCH_A"
+}
