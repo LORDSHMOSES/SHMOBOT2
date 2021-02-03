6c0f5275d438d29f04ae07019c6a8c71495c8619
Nathan Erickson <33501532+LORDSHMOSES@users.noreply.github.com>
2021-02-02 20:30:13 -0600

    Converted from discord client to discord bot.

--- bot.py
+++ bot.py
@@ -1,54 +1,47 @@
 # import libraries
-import discord
+from discord.ext import commands
 import r6sapi
 import getpass
 
+# get uplay username and password for API
+username = input("Username: ")
+password = getpass.getpass('Password: ')
+
 # get client object
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-
-# Event Listener for when bot comes online
-@client.event
-async def on_ready():
-    print('Logged in as {0.user}'.format(client))
-    # get uplay username and password for API
-    username = input("Username: ")
-    password = getpass.getpass('Password: ')
-
-# prevent bot from reading its own messages
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-
+# TODO: insert new user dm as a string below
 newuserdm = "Welcome!"
 
 
-# help printout
-# TODO: make a help message.
-@client.event
-async def on_message(message):
-    if message.content.startswith("!help"):
-        await message.channel.send(
-            "Nothing here yet!"
-        )
-
-
 # stat lookup command
-@client.event
-async def on_message(message):
-    if message.content.startswith("!stat"):
-        await message.channel.send(
-            "Nothing here yet!"
-        )
+@bot.command(help="Looks up the stats for a player")
+async def stat(ctx):
+    await ctx.send(
+        "Nothing here yet!"
+    )
+    auth = r6sapi.Auth(username, password)
+    # This is a test to prove the API works in this context.
+    player = await auth.get_player("LORD_SHMOSES", r6sapi.Platforms.UPLAY)
+    operator = await player.get_operator("mute")
+    print(operator.kills)
+    # TODO: gather stats here.
+    await auth.close()
 
+
+# Events
 
 # send welcome dm to new users!
 # TODO: testing
-@client.event
+@bot.event
 async def on_member_join(member):
     await member.send(newuserdm)
 
 
-client.run('CODEGOESHERE')
+# Event Listener for when bot comes online
+@bot.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(bot))
+
+
+bot.run('TOKENGOESHERE')
