e493cc581f63cb895c3763cf22375ac3e66ee9fd
Nathan Erickson <33501532+LORDSHMOSES@users.noreply.github.com>
2021-02-04 14:09:57 -0600

    added ranked k/d

--- bot.py
+++ bot.py
@@ -18,15 +18,16 @@ newuserdm = "Welcome!"
 # stat lookup command
 @bot.command(help="Looks up the stats for a player")
 async def stat(ctx, arg):
-    #TODO:add handler for null arg
+    # TODO:add handler for null arg
     auth = r6sapi.Auth(username, password)
     player = await auth.get_player(arg, r6sapi.Platforms.UPLAY)
     await r6sapi.Player.load_general(player)
+    await r6sapi.Player.load_queues(player)
     region = r6sapi.RankedRegions.NA
     Rank = await player.get_rank(region)
-    kills = player.kills
-    deaths = player.deaths
-    kd = (round((kills / deaths), 2))
+    rankstats = player.ranked
+    rankkd = (round((rankstats.kills / rankstats.deaths), 2))
+    kd = (round((player.kills / player.deaths), 2))
     mmr = Rank.mmr
     rankurl = Rank.get_icon_url()
     embed = discord.Embed(colour=discord.Colour(0xe75e15), description="Here are the stats for %s" % arg)
@@ -35,7 +36,8 @@ async def stat(ctx, arg):
     embed.set_thumbnail(url=rankurl)
     embed.set_author(name=arg, url=newurl)
 
-    embed.add_field(name="K/D", value=kd)
+    embed.add_field(name="Overall K/D", value=kd)
+    embed.add_field(name="Ranked K/D", value=rankkd)
     embed.add_field(name="Top Operators", value="op1, op2, op3")
     embed.add_field(name="Current MMR", value=mmr)
 
@@ -65,4 +67,4 @@ async def on_ready():
     print('Logged in as {0.user}'.format(bot))
 
 
-bot.run('CLIENTCODEHERE')
+bot.run('INSERTCODEHERE')
